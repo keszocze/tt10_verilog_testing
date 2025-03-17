a677433aa9221a02e6ced6732a90bca8fddbc11e
Oliver Keszöcze <olike@dtu.dk>
2025-03-17 13:58:52 +0100

    change the damn test file module name

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_keszocze_ssmcl user_project (
+  tt_um_verilog_testing user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
